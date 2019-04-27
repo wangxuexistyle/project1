127c4063fa26955ac3f7ef34496ce8eae95ecad6
wangxuexi <xuexistyle@foxmail.com>
2019-04-27 15:13:37 +0800

    第一次更新

--- src/main/java/com/xuexistyle/HelloWorld.java
+++ src/main/java/com/xuexistyle/HelloWorld.java
@@ -3,5 +3,7 @@ package com.xuexistyle;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello,world...");
+
+        System.out.println("first update...");
     }
 }
